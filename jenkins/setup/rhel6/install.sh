09dedfab7a2dd1c83032f8cbb7a5c829ab90f099
ayapapa <ayapapajapan@yahoo.co.jp>
2016-02-20 11:44:22 +0900

    add default hostname(#58)

--- jenkins/setup/rhel6/install.sh
+++ jenkins/setup/rhel6/install.sh
@@ -7,8 +7,7 @@ yum install -y java-1.7.0-openjdk-devel
 rpm -vi --force inst-script/rhel6/mod_auth_mysql-3.0.0-11.el6.1.redmine.x86_64.rpm
 
 #CHK=`grep "dport 8080" /etc/sysconfig/iptables`
-#if [ "$CHK" = '' ]
-#then
+#if [ "$CHK" = '' ]; then
 #  RULENUM=`iptables-save |grep INPUT |grep -n "dport 22"|awk -F : '{print $1}'`
 #  iptables -I  INPUT ${RULENUM} -p tcp -m state --state NEW -m tcp --dport 8080 -j ACCEPT
 #  iptables-save > /etc/sysconfig/iptables
