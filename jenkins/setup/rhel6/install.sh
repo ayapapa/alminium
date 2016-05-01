4c922e390d97dc237798bcef874dee6617d0162d
ayapapa <ayapapajapan@yahoo.co.jp>
2016-05-01 16:45:15 +0900

    modified jenkins installation for issue #64

--- jenkins/setup/rhel6/install.sh
+++ jenkins/setup/rhel6/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+yum clean all
 yum install -y jenkins
 yum install -y java-1.7.0-openjdk-devel
 rpm -vi --force inst-script/rhel6/mod_auth_mysql-3.0.0-11.el6.1.redmine.x86_64.rpm
