1d4504d6ed1e3a0e61dc79ca7de6b4f7212cb4e8
ayapapa <ayapapajapan@yahoo.co.jp>
2016-10-15 21:10:27 +0900

    update jenkins install script for #82

--- jenkins/setup/debian/install.sh
+++ jenkins/setup/debian/install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-wget -q -O - http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key | sudo apt-key add -
-echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
-apt-get -q2 update
-apt-get -y install jenkins
-
+wget -O cache/jenkins.deb http://pkg.jenkins-ci.org/debian-stable/binary/jenkins_1.651.3_all.deb
+dpkg -i cache/jenkins.deb
 sed -i 's/JENKINS_ARGS="--webroot/JENKINS_ARGS="--prefix=\/jenkins --webroot/' /etc/default/jenkins
 
 service jenkins restart
