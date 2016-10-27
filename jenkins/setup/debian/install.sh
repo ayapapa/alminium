988d76487c6c103cd04ddd95105fb15084ab5f8d
ayapapa <ayapapajapan@yahoo.co.jp>
2016-10-27 22:28:24 +0900

    update for jenkins install error(#82)

--- jenkins/setup/debian/install.sh
+++ jenkins/setup/debian/install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# install depend packages
+apt-get install openjdk-9-headless daemon
+
+# download and install jenkins 
 wget -O cache/jenkins.deb http://pkg.jenkins-ci.org/debian-stable/binary/jenkins_1.651.3_all.deb
 dpkg -i cache/jenkins.deb
 
+# avoid issue https://issues.jenkins-ci.org/browse/JENKINS-23232
+# and   issue https://issues.jenkins-ci.org/browse/JENKINS-35197
 sed -i 's/JAVA_ARGS="-Djava.awt.headless/JAVA_ARGS="-Dhudson.diyChunking=false -Djava.awt.headless/' /etc/default/jenkins
 sed -i 's/JENKINS_ARGS="--webroot/JENKINS_ARGS="--prefix=\/jenkins --webroot/' /etc/default/jenkins
 
+# restart jenkins
 service jenkins restart
