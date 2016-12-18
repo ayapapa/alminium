e56d9c42e46b936626abca6b6c5333f8ef21f370
ayapapa <ayapapajapan@yahoo.co.jp>
2016-12-18 11:01:02 +0900

    revert DB user in createdb.sql(#93)

--- redmine/setup/createdb.sql
+++ redmine/setup/createdb.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE alminium DEFAULT CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON alminium.* TO alminium@'%' IDENTIFIED BY 'alminium';
+GRANT ALL PRIVILEGES ON alminium.* TO alminium@localhost IDENTIFIED BY 'alminium';
