b4a3cbad62c1dc1c462064691c4d0a51032ce7bb
ayapapa <ayapapajapan@yahoo.co.jp>
2016-12-19 23:27:29 +0900

    added smelt options, those are db-host and it's root password. and added DB(alminium) user('alminium'@'%')(#93)

--- redmine/setup/createdb.sql
+++ redmine/setup/createdb.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE alminium DEFAULT CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON alminium.* TO alminium@localhost IDENTIFIED BY 'alminium';
+GRANT ALL PRIVILEGES ON alminium.* TO 'alminium'@'localhost' IDENTIFIED BY 'alminium';
+GRANT ALL PRIVILEGES ON alminium.* TO 'alminium'@'%' IDENTIFIED BY 'alminium';
