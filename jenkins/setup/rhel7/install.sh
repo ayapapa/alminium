97615ac595a961c8e1f2bb49fc45e43cd4d224b5
ayapapa <ayapapajapan@yahoo.co.jp>
2017-02-27 21:33:47 +0900

    update jenkins installer for #105

--- jenkins/setup/rhel7/install.sh
+++ jenkins/setup/rhel7/install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#wget -O cache/jenkins.rpm http://pkg.jenkins-ci.org/redhat-stable/jenkins-1.651.3-1.1.noarch.rpm
-#rpm -ivh cache/jenkins.rpm
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
