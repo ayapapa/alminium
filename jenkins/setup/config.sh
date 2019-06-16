91fc4e98f5100404ca23bbe8be5328dcf8cbc273
ayapapa <ayapapajapan@yahoo.co.jp>
2019-06-16 12:35:50 +0900

    modified for issue#133

--- jenkins/setup/config.sh
+++ jenkins/setup/config.sh
@@ -9,11 +9,19 @@ if [ ! -d ${JENKINS_PLUGIN_DIR} ]; then
 fi
 
 # try connect to jenkins service up
-curl localhost:8080/jenkins 2>/dev/null
+curl --noproxy localhost http://localhost:8080/jenkins 2>/dev/null
 
-# セキュリティ解除
-#sed -i.org "s/<useSecurity>true/<useSecurity>false/" /var/lib/jenkins/config.xml
-#service jenkins restart
+echo セキュリティ解除
+RET=-1
+until  [ "$RET" -eq "0" ]
+do
+  echo "waiting for ready..."
+  sleep 10
+  wget --no-proxy --spider http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar 2>/dev/null
+  RET=$?
+done
+sed -i.org "s/<useSecurity>true/<useSecurity>false/" /var/lib/jenkins/config.xml
+service jenkins restart
 
 # Jenkinsの設定を自動で進めるために初期ログインとアクセス権設定を手動で実施
 #echo "## Jenkinsの設定を継続するため以下を実施してください"
@@ -35,12 +43,13 @@ do
   RET=$?
 done
 
+# setting update center url
 wget -O /tmp/default.js http://updates.jenkins-ci.org/update-center.json
 
-# remove first and last line javascript wrapper
+## remove first and last line javascript wrapper
 sed '1d;$d' /tmp/default.js > /tmp/default.json
 
-# Now push it to the update URL
+## Now push it to the update URL
 #curl --noproxy localhost -X POST -H "Accept: application/json" -d @/tmp/default.json http://localhost:8080/jenkins/updateCenter/byId/default/postBack --verbose
 if [ ! -e /var/lib/jenkins/updates ]; then
   mkdir -p /var/lib/jenkins/updates
@@ -49,6 +58,7 @@ cp -f /tmp/default.json /var/lib/jenkins/updates/
 
 # Jenkinsのプロキシ設定
 if [ x"$http_proxy" != x"" ]; then
+  echo "** Jenkinsのプロキシ設定 **"
   # set proxy. sorry IPv4 only and user:pass not supported...
   proxyuser=`echo $http_proxy | sed -n 's/.*:\/\/\([a-zA-Z0-9]*\):.*/\1/p'`
   proxypass=`echo $http_proxy | sed -n 's/.*:\/\/[a-zA-Z0-9]*:\([a-zA-Z0-9:]*\)\@.*/\1/p'`
@@ -65,18 +75,19 @@ if [ x"$http_proxy" != x"" ]; then
   echo proxyserver=$proxyserver
   echo proxyport=$proxyport
 
-  curl --noproxy localhost -X POST --data "json={\"name\": \"$proxyserver\", \"port\": \"$proxyport\", \"userName\": \"$proxyuser\", \"password\": \"$proxypass\", \"noProxyHost\": \"\"}" http://localhost:8080/jenkins/pluginManager/proxyConfigure --verbose
+  # get crumb
+  jenkins_crumb=`curl --noproxy localhost 'http://localhost:8080/jenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
+  echo "*** Jenkins Crumb = ${jenkins_crumb} ***"
+  curl --noproxy localhost -X POST -H "${jenkins_crumb}" \
+       --data "json={\"name\": \"$proxyserver\", \"port\": \"$proxyport\", \"userName\": \"$proxyuser\", \"password\": \"$proxypass\", \"noProxyHost\": \"\"}" \
+       http://localhost:8080/jenkins/pluginManager/proxyConfigure --verbose
   RET=$?
   if [ "$RET" -ne "0" ]; then
     echo "proxy setting for jenkins fail"
     exit 1
   fi
-  #service jenkins restart
+  service jenkins restart
 fi
-
-echo セキュリティ解除
-sed -i.org "s/<useSecurity>true/<useSecurity>false/" /var/lib/jenkins/config.xml
-service jenkins restart
 
 # プラグインインストール
 sleep 10
@@ -91,7 +102,6 @@ install_jenkins_plugins() {
       proxy_opt=`echo ${proxy_opt} -Dhttp.proxyPort=${proxyport} -Dhttps.proxyPort=${proxyport}`
     fi
   fi
-  echo "** java proxy options = ${proxy_opt}"
   if [ ! -d ${JENKINS_PLUGIN_DIR}/${plugin_name} ]; then
     local result=-1
     until  [ "${result}" -eq "0" ]; do
@@ -137,7 +147,7 @@ sleep 30
 # Redmineログイン連携設定（手動）
 echo "## Jenkinsの設定が終わりました。"
 echo "## 初期化処理を以下の手順で実施してください。"
-echo "## RedmineユーザーでJenkinsへログインできるようにしない場合は、3～7の代わりにお好みの設定をしてください。"
+echo "## 3～7はRedmineユーザーでJenkinsへログインできるようにする場合に実施してください。"
 echo "## ※ブラウザで表示エラーになる場合は、しばらく待ってから以下を実施してください。"
 echo "## 1. ブラウザでhttp://${ALM_HOSTNAME}/jenkinsを表示"
 echo "## 2. 指示に従い初期設定を実施"
