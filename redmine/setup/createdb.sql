e96150373c195c90dfc6c760d88e80507bdac2cc
ayapapa <ayapapajapan@yahoo.co.jp>
2020-10-05 23:40:10 +0900

    issue #137 対応：PROCESS権限を設定するようにした

--- redmine/setup/createdb.sql
+++ redmine/setup/createdb.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE alminium DEFAULT CHARACTER SET utf8;
 GRANT ALL PRIVILEGES ON alminium.* TO 'alminium'@'localhost' IDENTIFIED BY 'alminium';
 GRANT ALL PRIVILEGES ON alminium.* TO 'alminium'@'%' IDENTIFIED BY 'alminium';
+GRANT PROCESS ON *.* TO 'alminium'@'localhost' IDENTIFIED BY 'alminium';
+GRANT PROCESS ON *.* TO 'alminium'@'%' IDENTIFIED BY 'alminium';
